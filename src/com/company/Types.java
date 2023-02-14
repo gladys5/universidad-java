c9094ece23a0bb8ba5ec88fc632fa94d9dd283d1
gladys5 <yumanji@live.com>
2023-02-13 23:57:26 -0700

    tipos de datos

---
+++ src/com/company/Types.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Types {
+    public static void main(String[] args) {
+/*
+variables
+* int: son los numeros enteros negativos y positivos
+* float:hace referencia a valores flotantes :decimales :1.2
+* double: son decimales :1,2,4,5
+* String:cadenas de texto
+* char :solo guarda un caracter
+* boolean:true o false
+*
+* */
+     int number =100;
+     System.out.println(number);
+        String sayHello = "Hello Word";
+        System.out.print(sayHello);
+        float number_float=2.5f;
+        System.out.println(number_float);
+        double number_double=2.5;
+        System.out.println(number_double);
+        String text="cadena de texto";
+        System.out.println(number_float);
+
+
+    }
+}
