1757fb7ba30905063ee914bfc293fcae5d295c2b
gladys5 <yumanji@live.com>
2023-02-15 23:27:03 -0700

    primitivos

---
+++ src/com/company/Primitives.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Primitives {
+    public static void main(String args[]){
+        /*
+        *tipos primitivos enteros: byte,short,int,long
+        */
+    }
+}
